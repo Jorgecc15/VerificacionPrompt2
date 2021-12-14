540043e043e763e8f000f116c3e39369f9658bd5
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2021-12-14 18:52:43 -0500

    Create verificacion.js

---
+++ verificacion.js
@@ -0,0 +1,30 @@
+let peso = Number (prompt("ingrese su peso en Kg"))
+let altura = Number (prompt("ingrese su altura en m"))
+let imc = peso/(altura*altura)
+if(imc< 18.5)
+    { respuesta="usted esta bajo de peso"
+}else{
+    if(imc <25)
+    { respuesta="su peso es normal"
+}else{
+     if(imc < 30)
+    { respuesta="preobesidad"
+ }else{
+     if(imc ==30)
+    { respuesta="obesidad"
+ }else{
+     if(imc<35 )
+     { respuesta="obesidad de clase 1"
+ }else{
+     if(imc < 40)
+     { respuesta="obesidad de clase 2"
+ }else{
+      respuesta="obesidad de clase 3"
+ }
+ }
+ }
+    
+}    
+}
+}
+document.getElementById("parrafopeso").innerHTML=respuesta
