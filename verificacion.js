3373b4506543897c0cfbd3004481393ae78d1952
Jorgecc15 <92415699+Jorgecc15@users.noreply.github.com>
2022-03-08 15:26:39 -0500

    Update verificacion.js

--- verificacion.js
+++ verificacion.js
@@ -1,9 +1,9 @@
 do{
     peso = Number (prompt("ingrese su peso en Kg"))
-} while (isNaN (peso))
+} while (isNaN (peso) || peso<0)
 do{
     altura = Number (prompt("ingrese su altura en m"))
-} while (isNaN (altura))
+} while (isNaN (altura) || peso<0)
 let altura2 = altura*altura
 let imc = peso / altura2
 if(imc< 18.5)
